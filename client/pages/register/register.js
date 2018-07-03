ddf4e82716f8db126be27f7ad87a9cf90dbc978e
Jiongsheng Cai <johson@Jiongshengs-MacBook-Pro.local>
2018-07-03 15:11:54 +0800

    started with server side, login success

---
+++ client/pages/register/register.js
@@ -0,0 +1,106 @@
+// pages/register.js
+const app = getApp()
+var qcloud = require('../../vendor/wafer2-client-sdk/index')
+var config = require('../../config')
+
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    userinfo:{}
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true,
+      })
+    }
+  },
+
+  registerUser: function(e) {
+    console.log(e)
+
+    // 调用登录接口
+    qcloud.login({
+      success(result) {
+        if (result) {
+          console.log('登录成功')
+        } else {
+          // 如果不是首次登录，不会返回用户信息，请求用户信息接口获取
+          qcloud.request({
+            url: config.service.requestUrl,
+            login: true,
+            success(result) {
+              console.log('登录成功')
+            },
+
+            fail(error) {
+              console.log('request fail', error)
+            }
+          })
+        }
+      },
+
+      fail(error) {
+        console.log('登录失败', error)
+      }
+    })
+
+    wx.setStorageSync('registered', true)
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
